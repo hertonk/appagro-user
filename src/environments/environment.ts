a9ea5e37b608b2fc1181056202a805768c285f60
Herton Freire Vilarim <hertonk@gmail.com>
2022-11-22 09:53:15 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,11 +11,12 @@ export const environment = {
   baseUrl: 'https://appagro.online/public/api/', // ex https://yourapi.com/public/api/ don't forgot to add public/api/ at the end
   imageUrl: 'https://appagro.online/public/storage/images/', // ex https://yourapi.com/public/storage/images/ don't forgot to add public/storage/images at the end
   firebase: {
-    apiKey: "YOURKEY",
-    authDomain: "YOURKEY",
-    projectId: "YOURKEY",
-    storageBucket: "YOURKEY",
-    messagingSenderId: "YOURKEY",
-    appId: "YOURKEY"
+     apiKey: "AIzaSyBbfFalcBk9_Xz1mQl26oIsqQIEMR3KVC8",
+      authDomain: "antonio-1b52f.firebaseapp.com",
+      projectId: "antonio-1b52f",
+      storageBucket: "antonio-1b52f.appspot.com",
+      messagingSenderId: "770004798772",
+      appId: "1:770004798772:web:a21dd07020b1b5aef9e961",
+      measurementId: "G-RBQL8Q2EWG"
   }
 };
